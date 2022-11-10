aa8651c5a1240d881b8cf7d4856fc94761b21513
Grayson Whitaker <100550253+glwhitaker@users.noreply.github.com>
2022-11-10 13:22:41 -0500

    Delete Lab directory

--- Lab/Lab2/calcTotalCharges.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALC_TOTAL_CHARGES
-#define CALC_TOTAL_CHARGES
-
-// prototype for calcTotalCharges (in-patient)
-double calcTotalCharges(double numDays, double roomRate, double hospServices, double medCharges);
-// prototype for calcTotalCharges (out-patient)
-double calcTotalCharges(double hospServices, double medCharges);
-
-#endif
--- Lab/Lab2/validateUserInput.h
+++
@@ -1,7 +0,0 @@
-#ifndef VALIDATE_USER_INPUT
-#define VALIDATE_USER_INPUT
-
-// prototype for validateUserInput
-bool validateUserInput(double userInput);
-
-#endif
